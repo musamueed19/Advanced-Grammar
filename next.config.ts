ff7cbc5857f179c5fff89f75341aa6d60ad57d2d
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-08-04 20:54:51 +0500

    final hopefully changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: "export",
+// };
 
-export default nextConfig;
+// export default nextConfig;
+const withPagefind = require("next-pagefind");
+
+const nextConfig = withPagefind({
+  // your existing config
+  output: "export", // required to generate HTML pages
+  experimental: {
+    appDir: true,
+  },
+});
+
+module.exports = nextConfig;
