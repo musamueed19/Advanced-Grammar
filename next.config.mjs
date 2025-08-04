cb26aacd13bd0b17dbc791bb5693fc80bf85fff4
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-08-04 17:26:54 +0500

    setup nextra

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import nextra from "nextra";
+
+// setup nextra with its configuration
+const withNextra = nextra({
+    // Add Nextra specific options here...
+})
+
+
+// export final Next.js config with Nextra included
+export default withNextra({
+    // Add regular Next.js configurations here...
+})
